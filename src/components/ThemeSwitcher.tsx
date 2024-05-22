d44b9400447b9e8b515efa25a069de56155dd5db
andr497 <andrangel97@gmail.com>
2024-05-21 22:22:41 -0600

    style: :art: Fixing styles from component ThemeSwitcher

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -20,20 +20,16 @@ const ThemeSwitcher = () => {
     };
     return (
         <button
-            className={`w-fit flex justify-center align-middle rounded-md hover:scale-110 active:scale-100 duration-200 dark:bg-[#212933]`}
+            className={`w-fit flex justify-center align-middle rounded-md hover:scale-110 active:scale-100 duration-200 bg-transparent`}
             onClick={handleClick}
         >
             {theme === "light" ? (
-                <>
-                    <MoonIcon width={25} />
-                    <span className="ml-1">Dark</span>
-                </>
+                <MoonIcon width={25} />
             ) : (
-                <>
-                    <SunIcon width={25} />
-                    <span className="ml-1">Light</span>
-                </>
+                <SunIcon width={25} />
             )}
+
+            <span className="ml-1 capitalize max-sm:hidden">{theme}</span>
         </button>
     );
 };
